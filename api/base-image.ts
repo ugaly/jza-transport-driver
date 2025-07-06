1c298048eecf9b87247f202eb28f15e14a4f0a47
jose <mwalwamaj@gmail.com>
2025-07-06 10:32:41 +0300

    changed base url

--- api/base-image.ts
+++ api/base-image.ts
@@ -1,2 +1,4 @@
-const base_Image = 'http://localhost:8000'
+// const base_Image = 'http://localhost:8000'
+const base_Image = 'https://jza.tz:8081'
+
 export default base_Image
--- api/base-url.ts
+++ api/base-url.ts
@@ -1,2 +1,4 @@
-const base_url = 'http://localhost:8000/api'
+// const base_url = 'http://localhost:8000/api'
+const base_url = 'https://jza.tz:8081/api'
+
 export default base_url
